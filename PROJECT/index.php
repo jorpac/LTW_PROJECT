9814b3320f16e811802e5a1c8f617ba80a91d22e
jorpac <36743869+jorpac@users.noreply.github.com>
2019-10-30 12:21:59 +0000

    First version//First prototype
    Started to build the main page, with grid.

---
+++ PROJECT/index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Book&GO</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="style.css">        
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css?family=Merriweather|Open+Sans+Condensed:300" rel="stylesheet">
+    </head>
+
+    <body>     
+        <header>
+            <h1><a href="index.php"><i class="fas fa-globe-africa"></i> BOOK&GO</a></h1>
+            <h2 id="login"><a href="login.php">Login</a></h2>
+            <h2 id="signup"><a href="login.php">Signup</a></h2>
+        </header>
+
+        <section id = "Top_places">
+
+        <div id ="Barcelona">
+            <img src="https://cdn.descontos.pt/imgOfertas/deals/original/D36396i11.jpg" alt="Barcelona city">
+            <p>Barcelona</p>
+            <p>x properties available</p>
+        </div>
+
+        <div id="Paris">
+            <img src="https://images.adsttc.com/media/images/5d44/14fa/284d/d1fd/3a00/003d/newsletter/eiffel-tower-in-paris-151-medium.jpg?1564742900" alt="Paris Eifell tower view">
+            <p>Paris</p>
+            <p>x properties available</p>
+        </div>
+
+        <div id = "Amsterdam">
+            <img src="https://img.traveltriangle.com/blog/wp-content/uploads/2018/06/amsterdam-evening-canal-cruise.jpg" alt="Amsterdam">
+            <p>Amsterdam</p>
+            <p>x properties available</p>
+        </div>
+
+        <div id="Lisbon">
+            <img src="https://www.lisbonguru.com/wp-content/uploads/2015/11/viewpoints-lisbon.jpg" alt="Lisbon">
+                <p>Lisbon</p>
+                <p>x properties available</p>
+        </div>
+
+        </section>
+    </body>
