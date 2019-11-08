bfc0b72dd6492aea37bafc0b71b5d56083ef09fa
jorpac <36743869+jorpac@users.noreply.github.com>
2019-11-08 10:53:06 +0000

    Delete action_search.php

--- PROJECT/action_search.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $city = $_POST['destination'];
-    include_once('Database/db_search.php');
-    $places = getPlaces($city);
-    // echo $places;
-    foreach ($places as $place) {
-        echo $place.['title'];
-    }
-?>
