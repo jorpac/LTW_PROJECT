5c349a5eed70aa819bbb03e7df1fd662cec4e19d
emma-mvn <emma.morvan@etu.emse.fr>
2019-12-06 11:31:56 +0000

    edit_house

--- Project/edit_house.php
+++ Project/edit_house.php
@@ -5,16 +5,10 @@ draw_header(NULL);
 
     <section id="house">
     <form method="post" action="edit_house_caract.php">
-        <input type="text" name="title" placeholder="new title" required>
-        <input type="text" name="description" placeholder="new description" required>
-        <input type="range" min="0" max="2000" step="10" list="list" name="price" place holder="new price" required>
-        <datalist id="list">
-            <option value="0" label="0">
-            <option value="500" label="500">
-            <option value="1000" label="1000">
-            <option value="1500" label="1500">
-            <option value="2000" label="2000">
-        </datalist>
+        <input type="text" name="title" placeholder="New title" required>
+        <input type="text" name="description" placeholder="New description" required>
+        <input type="range" id="rangeInput" min="0" max="2000" step="10" name="price" value="1000" required oninput="cost.value=rangeInput.value">
+        <input type="text" id="cost" name="price" placeholder="New price : 1000" for="rangeInput">
         <input type="submit" value="Save changes">
     </form>
     </section>
